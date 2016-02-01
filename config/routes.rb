bb40a655691798a60b64a572bd52a8183626ab9c
lucie galichon <lgalichon@gmail.com>
2016-02-01 18:39:08 +0100

    last version

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
 
+  get 'rsvp' => 'mariages#rsvp'
+
+  get 'mariage' => 'mariages#mariage'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
