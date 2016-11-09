6320ba641822a27648819539ce1b2e2f56e4e77b
lucie galichon <lgalichon@gmail.com>
2016-11-09 15:44:08 +0100

    remove senemaud

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery.countdown
-//= require_tree
 //= require lightbox
 //= require underscore
 //= require gmaps/google
 //= require swipebox
+//= require_tree
