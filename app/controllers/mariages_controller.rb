866c0d6f914fa99374604a4eeef061de4f41736b
lucie galichon <lgalichon@gmail.com>
2016-11-09 15:52:14 +0100

    remove senemaud name et photos

--- app/controllers/mariages_controller.rb
+++ app/controllers/mariages_controller.rb
@@ -1,6 +1,6 @@
 class MariagesController < ApplicationController
   def home
-    @photos = ["gegejean1.jpg","gegejean2.jpg","gegejean3.jpg","gegejean4.jpg","gegejean5.jpg","gegejean6.jpg","gegejean10.jpg","gegejean11.jpg" ]
+    @photos = ["gegejean1.jpg","gegejean2.jpg","gegejean3.jpg","gegejean4.jpg","gegejean5.jpg","gegejean6.jpg" ]
   end
 
   def about
